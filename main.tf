71b553a1980f462dd572c30788f8cb0a5060082e
Girija35 <girijasugu2000@gmail.com>
2024-10-26 16:08:11 +0530

    updating file2

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source             = "./vpc_module"
+module "vpc_module" {
+  source             = "./modules/vpc_module"
   cidr_block         = var.cidr_block
   public_subnet_cidr = var.public_subnet_cidr
   vpc_name           = var.vpc_name
 }
 
-module "ec2" {
-  source        = "./ec2_module"
+module "ec2_module" {
+  source        = "./modules/ec2_module"
   ami           = var.ami  # Replace with a valid AMI ID
   instance_type = var.instance_type
-  subnet_id     = module.vpc.public_subnet_id
+  subnet_id     = module.vpc_module.public_subnet_id
   instance_name = var.instance_name
 }
--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.vpc_module.vpc_id
 }
 
 output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
+  value = module.vpc_module.public_subnet_id
 }
 
 output "ec2_instance_id" {
-  value = module.ec2.instance_id
+  value = module.ec2_module.instance_id
 }
 
 output "ec2_public_ip" {
-  value = module.ec2.public_ip
+  value = module.ec2_module.public_ip
 }
