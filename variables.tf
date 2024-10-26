415861fbd71ebb86dc472cbbd043b58656b5d3fe
Girija35 <girijasugu2000@gmail.com>
2024-10-26 16:00:52 +0530

    updating file1

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
-var "cidr_block" {
+variable "cidr_block" {
     type=string
     default="10.0.0.0/16"
 }
-var "public_subnet_cidr" {
+variable "public_subnet_cidr" {
     type=string
     default="10.0.1.0/24"
 }
-var "vpc_name" {
+variable "vpc_name" {
     type=string
     default="example"
 }
-var "ami" {
+variable "ami" {
     type=string
     default="ami-0c55b159cbfafe1f0"
 }
-var "instance_type" {
+variable "instance_type" {
     type=string
     default="t2.micro"
 }
-var "instance_name" {
+variable "instance_name" {
     type=string
     default="example-ec2"
 }
